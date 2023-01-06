8eb589b31019265d432760a93814a15a687728da
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-01-06 09:35:45 +0530

    More changes

---
+++ main.js
@@ -0,0 +1,3 @@
+let copyright = document.querySelector(".copyright")
+copyright.innerHTML = `\n<span class="copyright">© 2023 <a href ="https://maheshtechnicals.com"  target "_blank">Mahesh Technicals</a></span> •`
+
