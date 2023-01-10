3b9250d44cda23e2a3fcd83b9a4536fb18d85fe0
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-01-10 20:34:41 +0530

    Added youtube channel style

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-let copyright = document.querySelector(".copyright")
-copyright.innerHTML = `\n<span class="copyright">© 2023 <a href ="https://maheshtechnicals.com"  target "_blank">Mahesh Technicals</a></span> •`
+let subscount = document.querySelector("#subscount")
+const user = "UCnaXfsmRmrdc5IprzOok43Q"
+const api = `AIzaSyAKJEP4Q19B7E0To3BwItSNDhYVooYf6Fo`
 
+
+
+getsubs = () => {
+
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${user}&key=${api}`)
+    .then((res) => {
+      return res.json()
+    })
+
+    .then((data) => {
+     let subs =data.items[0].statistics.subscriberCount
+
+  subscount.innerText =subs
+    })
+
+    .catch((error) => {
+      console.log(error)
+    })
+
+}
+
+
+getsubs()
